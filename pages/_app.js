0f79d6240955f32b9c8b280ae2e956abc5f5c004
anil-masetti <masettianilkumar.com>
2022-02-23 10:55:00 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
+// pages/_app.js
+import { SessionProvider } from "next-auth/react";
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { QueryClientProvider, QueryClient } from 'react-query'
+
 import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const queryClient = new QueryClient()
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <QueryClientProvider client={queryClient}>
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+    <ReactQueryDevtools initialIsOpen={false} position='bottom-right'/>
+    </QueryClientProvider>
+  )
 }
-
-export default MyApp
